3d51f46147966f05151023b517e1bb5fdfd95776
Damian Olszewski <damian@ravendb.net>
2025-03-17 12:02:26 +0100

    RavenDB-23556 Connection strings: check if name is unique

--- src/Raven.Studio/typescript/components/pages/database/settings/connectionStrings/editForms/AzureQueueStorageConnectionString.tsx
+++ src/Raven.Studio/typescript/components/pages/database/settings/connectionStrings/editForms/AzureQueueStorageConnectionString.tsx
@@ -24,6 +24,8 @@ import { mapAzureQueueStorageConnectionStringSettingsToDto } from "components/pa
 import assertUnreachable from "components/utils/assertUnreachable";
 import { Icon } from "components/common/Icon";
 import { PopoverWithHover } from "components/common/PopoverWithHover";
+import { connectionStringSelectors } from "../store/connectionStringsSlice";
+import { connectionStringsUtils } from "../connectionStringsUtils";
 
 type FormData = ConnectionFormData<AzureQueueStorageConnection>;
 
@@ -36,6 +38,8 @@ export default function AzureQueueStorageConnectionString({
     isForNewConnection,
     onSave,
 }: AzureQueueStorageConnectionStringProps) {
+    const usedNames = useAppSelector(connectionStringSelectors.connections)["AzureQueueStorage"].map((x) => x.name);
+
     const { control, handleSubmit, trigger } = useForm<FormData>({
         mode: "all",
         defaultValues: getDefaultValues(initialConnection, isForNewConnection),
@@ -44,6 +48,8 @@ export default function AzureQueueStorageConnectionString({
                 data,
                 {
                     authType: data.authType,
+                    isForNewConnection,
+                    usedNames,
                 },
                 options
             ),
@@ -264,7 +270,7 @@ function getStringRequiredSchema(authType: AzureQueueStorageAuthenticationType) 
 }
 
 const schema = yupObjectSchema<FormData>({
-    name: yup.string().nullable().required(),
+    name: connectionStringsUtils.nameSchema,
     authType: yup.string<AzureQueueStorageAuthenticationType>(),
     settings: yupObjectSchema<FormData["settings"]>({
         connectionString: yupObjectSchema<FormData["settings"]["connectionString"]>({
