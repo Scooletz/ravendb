188639f0bbf7fd149bc22863117c9537e320f2b8
Szymon Kulec <szymon.kulec@ravendb.net>
2025-10-03 14:02:39 +0200

    RavenDB-25107 align JSON with requirements

--- src/Raven.Client/Documents/Operations/Replication/PullReplicationAsSink.cs
+++ src/Raven.Client/Documents/Operations/Replication/PullReplicationAsSink.cs
@@ -70,8 +70,16 @@ namespace Raven.Client.Documents.Operations.Replication
 #pragma warning disable CS0618 // Type or member is obsolete
             djv[nameof(HubDefinitionName)] = HubDefinitionName;
 #pragma warning restore CS0618 // Type or member is obsolete
-            djv[nameof(CertificateWithPrivateKey)] = CertificateWithPrivateKey;
-            djv[nameof(CertificatePassword)] = CertificatePassword;
+            if (CertificateWithPrivateKey != null)
+            {
+                djv[nameof(CertificateWithPrivateKey)] = CertificateWithPrivateKey;
+            }
+
+            if (CertificatePassword != null)
+            {
+                djv[nameof(CertificatePassword)] = CertificatePassword;
+            }
+
             djv[nameof(AllowedHubToSinkPaths)] = AllowedHubToSinkPaths;
             djv[nameof(AllowedSinkToHubPaths)] = AllowedSinkToHubPaths;
             djv[nameof(AccessName)] = AccessName;
