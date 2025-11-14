7f42861622b6d87a3842fdafed39e24ecc16556f
Szymon Kulec <scooletz@gmail.com>
2025-11-14 10:36:20 +0100

    Replace IDynamicJsonValueConvertible with IDynamicJson

--- src/Raven.Client/Documents/Operations/Replication/IExternalReplication.cs
+++ src/Raven.Client/Documents/Operations/Replication/IExternalReplication.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using Sparrow.Json;
 
 namespace Raven.Client.Documents.Operations.Replication
 {
-    internal interface IExternalReplication : IDynamicJsonValueConvertible
+    internal interface IExternalReplication : IDynamicJson
     {
         bool Disabled { get; set; }
 
