c851650b0c68620936233d43ec96ef77af93be92
Szymon Kulec <szymon.kulec@ravendb.net>
2025-11-17 11:39:17 +0100

    benchmark

--- bench/RequestHandler.Benchmark/Program.cs
+++ bench/RequestHandler.Benchmark/Program.cs
@@ -1,59 +1,6 @@
-using System;
-using System.Globalization;
-using System.Threading.Tasks;
+﻿// See https://aka.ms/new-console-template for more information
 
-namespace RequestHandler.Benchmark;
+using BenchmarkDotNet.Running;
+using RequestHandler.Benchmark;
 
-internal static class Program
-{
-    private const int DefaultIterations = 10_000;
-
-    private static async Task<int> Main(string[] args)
-    {
-        var (iterations, useRequestScope) = ParseArguments(args);
-
-        Console.WriteLine($"Starting RequestHandler harness with {iterations:N0} iterations (request scope: {useRequestScope}).");
-
-        await using var harness = await RequestHandlerHarness.CreateAsync(useRequestScope);
-
-        await harness.WarmupAsync();
-
-        var (elapsed, failures, lastFailure) = await harness.ExecuteAsync(iterations);
-
-        Console.WriteLine($"Completed {iterations:N0} iterations in {elapsed}. Failures: {failures:N0}.");
-        if (failures > 0)
-        {
-            if (lastFailure != null)
-                Console.WriteLine($"Last failure: {lastFailure}");
-            return 1;
-        }
-
-        var perRequest = elapsed.TotalMilliseconds / iterations;
-        Console.WriteLine($"Average per request: {perRequest:F3} ms");
-        return 0;
-    }
-
-    private static (int Iterations, bool UseRequestScope) ParseArguments(string[] args)
-    {
-        var iterations = DefaultIterations;
-        var useRequestScope = false;
-
-        foreach (var arg in args)
-        {
-            if (arg.StartsWith("--iterations=", StringComparison.OrdinalIgnoreCase))
-            {
-                var value = arg[("--iterations=").Length..];
-                if (int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var parsed) && parsed > 0)
-                    iterations = parsed;
-                continue;
-            }
-
-            if (string.Equals(arg, "--use-request-scope", StringComparison.OrdinalIgnoreCase))
-            {
-                useRequestScope = true;
-            }
-        }
-
-        return (iterations, useRequestScope);
-    }
-}
+BenchmarkSwitcher.FromAssembly(typeof(RequestContextScopingBenchmark).Assembly);
---
+++ test/BenchmarkTests/RequestHandlingTest.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Raven.Client.Documents;
+using Raven.Server;
+using Raven.Server.Web;
+using Raven.Tests.Core.Utils.Entities;
+using Tests.Infrastructure;
+using Xunit.Abstractions;
+
+namespace BenchmarkTests;
+
+public class RequestHandlingTest(ITestOutputHelper output) : BenchmarkTestBase(output)
+{
+    private const int Iterations = 1000;
+    private const string DocumentId = "users/1-A";
+
+    [RavenTheory(RavenTestCategory.None)]
+    [RavenData(DatabaseMode = RavenDatabaseMode.Single)]
+    public async Task Request_handling_routing(Options options)
+    {
+        using (var store = GetDocumentStore())
+        {
+            string databaseName = store.Database;
+            using (var session = store.OpenAsyncSession())
+            {
+                await session.StoreAsync(new User(), DocumentId);
+                await session.SaveChangesAsync();
+            }
+
+            RavenServer server = GetServers().Single();
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                DefaultHttpContext context = CreateHttpContext(server, databaseName);
+
+                // Test routing
+                using var requestHandlerContext = new RequestHandlerContext();
+                requestHandlerContext.HttpContext = context;
+
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                await server.Router.HandlePath(requestHandlerContext);
+            }
+        }
+    }
+
+    private static DefaultHttpContext CreateHttpContext(RavenServer server, string databaseName)
+    {
+        var context = new DefaultHttpContext
+        {
+            Request =
+            {
+                Method = HttpMethods.Get,
+                Scheme = "http",
+                Host = new HostString("127.0.0.1"),
+                Protocol = "HTTP/1.1",
+                Path = $"/databases/{databaseName}/docs",
+                QueryString = new QueryString($"?id={DocumentId}"),
+                Body = Stream.Null,
+                ContentLength = 0
+            }
+        };
+
+        context.Request.Headers.Host = context.Request.Host.Value;
+
+        // NullStream for responses
+        context.Response.Body = Stream.Null;
+        context.Features.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(Stream.Null));
+
+        context.Features.Set<IHttpConnectionFeature>(new HttpConnectionFeature
+        {
+            RemoteIpAddress = IPAddress.Loopback,
+            RemotePort = 0,
+            LocalIpAddress = IPAddress.Loopback,
+            LocalPort = 0
+        });
+
+        return context;
+    }
+
+    public override Task InitAsync(DocumentStore store) => Task.CompletedTask;
+}
