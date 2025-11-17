a005b91e7087998d5621ede31401965d3fc59128
Szymon Kulec <szymon.kulec@ravendb.net>
2025-11-17 12:25:52 +0100

    updated

--- bench/RequestHandler.Benchmark/Program.cs
+++ bench/RequestHandler.Benchmark/Program.cs
@@ -1,9 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
 using BenchmarkDotNet.Running;
 using RequestHandler.Benchmark;
 
-BenchmarkSwitcher.FromAssembly(typeof(Bench).Assembly);
-
-Console.WriteLine("TEST");
+BenchmarkSwitcher.FromAssembly(typeof(RequestContextScopingBenchmark).Assembly).Run(args);
--- bench/RequestHandler.Benchmark/RequestContextScopingBenchmark.cs
+++ bench/RequestHandler.Benchmark/RequestContextScopingBenchmark.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
@@ -7,90 +9,75 @@ using Raven.Server.Web;
 
 namespace RequestHandler.Benchmark;
 
-public class Bench
+[MemoryDiagnoser]
+public class RequestContextScopingBenchmark
 {
-    [Benchmark]
-    public int B() => 1;
+    private RavenDbInstance? _instance;
+
+    private const string DocumentId = "users/1-A";
+    private readonly QueryString _query = new($"?id={DocumentId}");
+    private static readonly PathString GetDocPath = $"/databases/{RavenDbInstance.DatabaseName}/docs";
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _instance = new RavenDbInstance();
+        _instance.InitializeDatabase();
+
+        using IDocumentSession session = _instance.Store.OpenSession();
+        session.Store(new {Name = "Test"}, DocumentId);
+        session.SaveChanges();
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        _instance.Dispose();
+    }
 
     [Benchmark]
-    public int V() => 1;
+    public async Task<int> Get_document()
+    {
+        DefaultHttpContext context = PrepareContext();
+
+        // Test routing
+        using var requestHandlerContext = new RequestHandlerContext();
+        requestHandlerContext.HttpContext = context;
+
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
+        await _instance!.Server.Router.HandlePath(requestHandlerContext);
+        return context.Response.StatusCode;
+    }
+
+    private DefaultHttpContext PrepareContext()
+    {
+        var context = new DefaultHttpContext
+        {
+            Request =
+            {
+                Method = HttpMethods.Get,
+                Scheme = "http",
+                Host = new HostString("127.0.0.1"),
+                Protocol = "HTTP/1.1",
+                Path = GetDocPath,
+                QueryString = _query,
+                Body = Stream.Null,
+                ContentLength = 0
+            }
+        };
+
+        context.Request.Headers.Host = context.Request.Host.Value;
+        
+        context.Response.Body = Stream.Null;
+        context.Features.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(Stream.Null));
+        context.Features.Set<IHttpConnectionFeature>(new HttpConnectionFeature
+        {
+            RemoteIpAddress = IPAddress.Loopback,
+            RemotePort = 0,
+            LocalIpAddress = IPAddress.Loopback,
+            LocalPort = 0
+        });
+
+        return context;
+    }
 }
-
-// [ShortRunJob]
-// public class RequestContextScopingBenchmark
-// {
-//     private RavenDbInstance? _instance;
-//
-//     private const string DocumentId = "users/1-A";
-//     private readonly QueryString _query = new($"?id={DocumentId}");
-//     private static readonly PathString GetDocPath = $"/databases/{RavenDbInstance.DatabaseName}/docs";
-//
-//     [GlobalSetup]
-//     public void GlobalSetup()
-//     {
-//         _instance = new RavenDbInstance();
-//         _instance.InitializeDatabase();
-//
-//         using IDocumentSession session = _instance.Store.OpenSession();
-//         session.Store(new {Name = "Test"}, DocumentId);
-//         session.SaveChanges();
-//     }
-//
-//     [GlobalCleanup]
-//     public void GlobalCleanup()
-//     {
-//         _instance.Dispose();
-//     }
-//
-//     [Benchmark]
-//     public async Task<int> Get_document()
-//     {
-//         DefaultHttpContext context = PrepareContext();
-//
-//         // Test routing
-//         using var requestHandlerContext = new RequestHandlerContext();
-//         requestHandlerContext.HttpContext = context;
-//
-//         context.Response.StatusCode = (int)HttpStatusCode.OK;
-//         await _instance!.Server.Router.HandlePath(requestHandlerContext);
-//         return context.Response.StatusCode;
-//     }
-//
-//     [Benchmark]
-//     public int A()
-//     {
-//         return 1;
-//     }
-//
-//     private DefaultHttpContext PrepareContext()
-//     {
-//         var context = new DefaultHttpContext
-//         {
-//             Request =
-//             {
-//                 Method = HttpMethods.Get,
-//                 Scheme = "http",
-//                 Host = new HostString("127.0.0.1"),
-//                 Protocol = "HTTP/1.1",
-//                 Path = GetDocPath,
-//                 QueryString = _query,
-//                 Body = Stream.Null,
-//                 ContentLength = 0
-//             }
-//         };
-//
-//         context.Request.Headers.Host = context.Request.Host.Value;
-//         
-//         context.Response.Body = Stream.Null;
-//         context.Features.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(Stream.Null));
-//         context.Features.Set<IHttpConnectionFeature>(new HttpConnectionFeature
-//         {
-//             RemoteIpAddress = IPAddress.Loopback,
-//             RemotePort = 0,
-//             LocalIpAddress = IPAddress.Loopback,
-//             LocalPort = 0
-//         });
-//
-//         return context;
-//     }
-// }
