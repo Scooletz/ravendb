bc1af1ba48d6267112e5a3c326c473d2d8bf7855
Michał Leśniak <michal.lesniak@ravendb.net>
2026-04-02 14:39:32 +0200

    RavenDB-21192 Return "The task is on a different node" instead of "NoSuchInstance" when accessing SNMP metrics on nodes without a queried task

--- src/Raven.Server/Monitoring/Snmp/Objects/DatabaseEtlScalarObjectBase.cs
+++ src/Raven.Server/Monitoring/Snmp/Objects/DatabaseEtlScalarObjectBase.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Pipeline;
 using Raven.Server.Documents;
 using Raven.Server.Documents.ETL;
 
@@ -10,12 +11,27 @@ public abstract class DatabaseEtlScalarObjectBase<TData> : DatabaseScalarObjectB
 {
     protected readonly string EtlName;
 
+    private static readonly OctetString TaskOnDifferentNode = new OctetString("The task is on a different node");
+
     protected DatabaseEtlScalarObjectBase(string databaseName, string etlName, DatabasesLandlord landlord, int databaseIndex, int etlIndex, string dots)
         : base(databaseName, landlord, string.Format(dots, databaseIndex), etlIndex)
     {
         EtlName = etlName;
     }
-    
+
+    public override ISnmpData Data
+    {
+        get
+        {
+            var database = GetDatabase();
+            if (database != null && GetEtl(database) == null)
+                return TaskOnDifferentNode;
+
+            return base.Data;
+        }
+        set => throw new AccessFailureException();
+    }
+
     protected EtlProcess GetEtl(DocumentDatabase database)
     {
         return database.EtlLoader.Processes.SingleOrDefault(x => x.Name == EtlName);
