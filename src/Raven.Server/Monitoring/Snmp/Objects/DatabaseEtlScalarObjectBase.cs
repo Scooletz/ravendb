f7dd014d816a6a94f3038b9dd25dec2abda0baa1
Michał Leśniak <michal.lesniak@ravendb.net>
2026-04-03 14:30:18 +0200

    RavenDB-21192 Removed TaskOnDifferentNode OctetString

--- src/Raven.Server/Monitoring/Snmp/Objects/DatabaseEtlScalarObjectBase.cs
+++ src/Raven.Server/Monitoring/Snmp/Objects/DatabaseEtlScalarObjectBase.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using Lextm.SharpSnmpLib;
-using Lextm.SharpSnmpLib.Pipeline;
 using Raven.Server.Documents;
 using Raven.Server.Documents.ETL;
 
@@ -11,25 +10,10 @@ public abstract class DatabaseEtlScalarObjectBase<TData> : DatabaseScalarObjectB
 {
     protected readonly string EtlName;
 
-    private static readonly OctetString TaskOnDifferentNode = new OctetString("The task is on a different node");
-
     protected DatabaseEtlScalarObjectBase(string databaseName, string etlName, DatabasesLandlord landlord, int databaseIndex, int etlIndex, string dots)
         : base(databaseName, landlord, string.Format(dots, databaseIndex), etlIndex)
     {
         EtlName = etlName;
-    }
-
-    public override ISnmpData Data
-    {
-        get
-        {
-            var database = GetDatabase();
-            if (database != null && GetEtl(database) == null)
-                return TaskOnDifferentNode;
-
-            return base.Data;
-        }
-        set => throw new AccessFailureException();
     }
 
     protected EtlProcess GetEtl(DocumentDatabase database)
