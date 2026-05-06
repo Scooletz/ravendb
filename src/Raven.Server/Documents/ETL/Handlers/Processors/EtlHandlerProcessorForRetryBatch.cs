6b6ea4a865ebe2c6111008d1cea3ce7176422cc8
Michał Leśniak <michal.lesniak@ravendb.net>
2026-05-06 09:21:19 +0200

    RavenDB-21192 Return 404 on attempt to retry batch of non-existing process

--- src/Raven.Server/Documents/ETL/Handlers/Processors/EtlHandlerProcessorForRetryBatch.cs
+++ src/Raven.Server/Documents/ETL/Handlers/Processors/EtlHandlerProcessorForRetryBatch.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Raven.Server.ServerWide;
 using Raven.Server.ServerWide.Context;
@@ -13,13 +14,19 @@ internal sealed class EtlHandlerProcessorForRetryBatch : AbstractEtlHandlerProce
     }
 
     protected override bool SupportsCurrentNode => true;
-    
+
     protected override ValueTask HandleCurrentNodeAsync()
     {
         var etlProcessName = GetEtlProcessName();
 
         var process = RequestHandler.Database.EtlLoader.Processes.SingleOrDefault(x => x.Name == etlProcessName);
-        process?.ForceBatchRetry();
+        if (process == null)
+        {
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            return ValueTask.CompletedTask;
+        }
+
+        process.ForceBatchRetry();
 
         return ValueTask.CompletedTask;
     }
