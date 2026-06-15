ab8265a2aef317db23655166da7991e730175718
Michał Leśniak <michal.lesniak@ravendb.net>
2026-06-15 11:15:28 +0200

    RavenDB-26024 Update RavenDB_17478 to expect 0A000 declare-function-fragment diagnostic

--- test/EmbeddedTests/Server/Integrations/PostgreSQL/RavenDB_17478.cs
+++ test/EmbeddedTests/Server/Integrations/PostgreSQL/RavenDB_17478.cs
@@ -1,5 +1,4 @@
 ﻿#if RUN_NPGSQL_TESTS
-using System;
 using System.Threading.Tasks;
 using Npgsql;
 using Xunit;
@@ -9,7 +8,7 @@ namespace EmbeddedTests.Server.Integrations.PostgreSQL
     public class RavenDB_17478 : PostgreSqlIntegrationTestBase
     {
         [Fact]
-        public async Task ShouldRejectUnsupportedRqlQuery_WithUnhandledQueryError()
+        public async Task ShouldRejectDeclareFunctionWithSemicolonsInBody_WithJsFragmentDiagnostic()
         {
             const string query =
                 @"declare function name(e) {
@@ -26,9 +25,9 @@ namespace EmbeddedTests.Server.Integrations.PostgreSQL
             {
                 var pgException = await Assert.ThrowsAsync<PostgresException>(async () => await Act(store, query));
 
-                Assert.Equal("54001", pgException.SqlState);
-                Assert.StartsWith($"Unhandled query:{Environment.NewLine}", pgException.MessageText);
-                Assert.Contains("declare function name", pgException.MessageText);
+                Assert.Equal("0A000", pgException.SqlState);
+                Assert.Contains("declare function", pgException.MessageText);
+                Assert.Contains("Remove the semicolons from the JS body", pgException.MessageText);
             }
         }
     }
